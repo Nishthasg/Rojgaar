e62f30183fac0dd8beafac35f5aa8dec6b73b008
Nishtha Singh <nishtha.singh@pactap.com>
2026-03-03 11:57:50 +0530

    3/3/26

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ForgetComponent } from './pages/forget/forget.component';
+import { WorkersComponent } from './pages/workers/workers.component';
+import { WorkerDetailsComponent } from './pages/workers/worker-details/worker-details.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgetComponent,
+    WorkersComponent,
+    WorkerDetailsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
